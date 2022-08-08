1768f768268d50552652d7e7b80204fe171aff28
tao <2966295710@qq.com>
2022-08-08 20:28:34 +0800

    修改selectSort

--- src/main/java/nt/study/sort/impl/SelectSort.java
+++ src/main/java/nt/study/sort/impl/SelectSort.java
@@ -5,7 +5,6 @@ import nt.study.sort.Sort;
 public class SelectSort implements Sort {
 
     public  void sort(int[] arr){
-        // 1. 找到无序区最小的数 将其与无序区的第一个数进行交换
         for (int i = 0; i < arr.length; i++) {
             int index = i;
             for (int j = i + 1; j < arr.length; j++) {
